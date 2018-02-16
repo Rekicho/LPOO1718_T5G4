820e79141d0ef1390e1d97622e2d6378c6d8925d
Bruno Sousa <brunorato12@hotmail.com>
2018-02-16 14:02:20 +0000

    Ogre now steps over key

--- Projeto1/src/Game.java
+++ Projeto1/src/Game.java
@@ -4,9 +4,9 @@ import java.util.Random;
 
 public class Game 
 {
-	
-	public static int level1() {
-		Scanner s = new Scanner(System.in);
+
+	public static int level1(Scanner s) 
+	{
 
 		char[][] map = { {'X','X','X','X','X','X','X','X','X','X'},
 				{'X','H',' ',' ','I',' ','X',' ','G','X'},
@@ -18,7 +18,7 @@ public class Game
 				{'X','X','X',' ','X','X','X','X',' ','X'},
 				{'X',' ','I',' ','I',' ','X','k',' ','X'},
 				{'X','X','X','X','X','X','X','X','X','X'} };
-		
+
 		char[] movg = {'a','s','s','s','s','a','a','a','a','a','a','s','d','d','d','d','d','d','d','w','w','w','w','w'};
 
 		int xpos = 1;
@@ -33,6 +33,7 @@ public class Game
 
 		int flag = -1;
 		int gcounter = 0;
+		boolean lever = false;
 
 		while(flag == -1)
 		{	
@@ -65,10 +66,10 @@ public class Game
 
 			case 'q':
 			case 'Q': flag = 0; continue;
-			
+
 			default: continue;
 			}
-			
+
 			switch(movg[gcounter%movg.length])
 			{
 			case 'w': gxspeed = 0; gyspeed = -1; break;
@@ -76,34 +77,51 @@ public class Game
 			case 's': gxspeed = 0; gyspeed = 1; break;
 			case 'd': gxspeed = 1; gyspeed = 0; break;
 			}
-			
+
 			if (map[ypos + yspeed][xpos + xspeed] == 'S')
 				flag = 1;
 
 			if (map[ypos + yspeed][xpos + xspeed] != 'X' && map[ypos + yspeed][xpos + xspeed] != 'I')
 			{
+				if(lever)
+					lever = false;
+
 				if(map[ypos + yspeed][xpos + xspeed] == 'k')
+				{
+					lever = true;
+
 					for(int i = 0; i < map.length; i++)
 						if(map[i][0] == 'I')
 							map[i][0] = 'S';
+				}
 
-				map[ypos][xpos] = ' ';
-				map[ypos + yspeed][xpos + xspeed] = 'H';
-				
-				xpos += xspeed;
-				ypos += yspeed;
+				if(!lever)
+				{
+					map[ypos][xpos] = ' ';
+
+					map[ypos + yspeed][xpos + xspeed] = 'H';
+
+					xpos += xspeed;
+					ypos += yspeed;
+				}
+
+				if((gxpos + gxspeed == xpos) && (gypos + gyspeed == ypos))
+				{
+					flag = 0;
+					break;
+				}
 				
 				map[gypos][gxpos] = ' ';
 				map[gypos + gyspeed][gxpos + gxspeed] = 'G';
-				
+
 				gxpos += gxspeed;
 				gypos += gyspeed;
-				
+
 				gcounter++;
-				
+
 			}
-			
-			if ((Math.abs(gypos - ypos) == 1 && gxpos == xpos) || (Math.abs(gxpos - xpos) == 1 && gypos == ypos))
+
+			if (flag != 1 && ((Math.abs(gypos - ypos) == 1 && gxpos == xpos) || (Math.abs(gxpos - xpos) == 1 && gypos == ypos)))
 				flag = 0;
 		}
 
@@ -117,20 +135,17 @@ public class Game
 
 		if(flag == 0)
 			System.out.println("GameOver!");
-		
-		s.close();
-		
+
 		return flag;
 	}
-	
-	public static int level2() {
-		Scanner s = new Scanner(System.in);
-		
+
+	public static int level2(Scanner s) {
+
 		Random rng = new Random();
 
 		char[][] map = { 
 				{'X','X','X','X','X','X','X','X','X'},
-				{'I',' ',' ',' ','O',' ',' ','k','X'},
+				{'I',' ',' ',' ','0',' ',' ','k','X'},
 				{'X',' ',' ',' ',' ',' ',' ',' ','X'},
 				{'X',' ',' ',' ',' ',' ',' ',' ','X'},
 				{'X',' ',' ',' ',' ',' ',' ',' ','X'},
@@ -139,19 +154,19 @@ public class Game
 				{'X','H',' ',' ',' ',' ',' ',' ','X'},
 				{'X','X','X','X','X','X','X','X','X'} 
 		};
-		
+
 		int flag = -1;
-		
+
 		int xpos = 1;
 		int ypos = 7;
 		int xspeed = 0;
 		int yspeed = 0;
-		
+
 		int oxpos = 4;
 		int oypos = 1;
-		
+
 		char player = 'H';
-		
+
 		while(flag == -1)
 		{	
 			System.out.print("\n\n\n\n\n\n\n");
@@ -183,76 +198,109 @@ public class Game
 
 			case 'q':
 			case 'Q': flag = 0; continue;
-			
+
 			default: continue;
 			}
-			
+
 			if (map[ypos + yspeed][xpos + xspeed] == 'S')
 				flag = 1;
-			
-			if (map[ypos + yspeed][xpos + xspeed] == 'I' && player == 'K') {
+
+			boolean waste = false;
+
+			if (map[ypos + yspeed][xpos + xspeed] == 'I' && player == 'K') 
+			{
+				waste = true;
 				map[ypos + yspeed][xpos + xspeed] = 'S';
 			}
-			
+
 			if (map[ypos + yspeed][xpos + xspeed] != 'X' && map[ypos + yspeed][xpos + xspeed] != 'I')
 			{
-				if(map[ypos + yspeed][xpos + xspeed] == 'k')
-					player = 'K';
+				if(!waste)
+				{
+					if(map[ypos + yspeed][xpos + xspeed] == 'k')
+						player = 'K';
 
-				map[ypos][xpos] = ' ';
-				map[ypos + yspeed][xpos + xspeed] = player;
-				
-				xpos += xspeed;
-				ypos += yspeed;
-				
+					map[ypos][xpos] = ' ';
+					map[ypos + yspeed][xpos + xspeed] = player;
+
+					xpos += xspeed;
+					ypos += yspeed;
+				}
+
 				int r1;
 				int r2;
 
 				boolean randomFlag = true;
-				
+
 				while (randomFlag) {
-				
+
 					r1 = rng.nextInt(2);
 					r2 = rng.nextInt(2);
-					
-					System.out.println(r1);
-					System.out.println(r2);
-					
+
 					int mov = 1;
-					
+
 					if (r2 == 0)
 						mov = -1;
-					
+
 					if (r1 == 0) {
-						if (map[oypos + mov][oxpos] == ' ') {
-							
+						if (map[oypos + mov][oxpos] != 'I' && map[oypos + mov][oxpos] != 'X') {
+
 							randomFlag = false;
 							
-							map[oypos][oxpos] = ' ';
-							map[oypos + mov][oxpos] = 'O';
-							oypos += mov;
+							if((oxpos == xpos) && (oypos + mov == ypos))
+							{
+								flag = 0;
+								break;
+							}
+
 							
+							if (map[oypos][oxpos] == '$')
+								map[oypos][oxpos] = 'k';
+							
+							else map[oypos][oxpos] = ' ';
+							
+							if(map[oypos + mov][oxpos] == 'k')
+								map[oypos + mov][oxpos] = '$';
+							
+							else map[oypos + mov][oxpos] = '0';
+							
+							oypos += mov;
+
 						}	
 					} else {
-						if (map[oypos][oxpos + mov] == ' ') {
-							
+						if (map[oypos][oxpos + mov] != 'I' && map[oypos][oxpos + mov] != 'X') {
+
 							randomFlag = false;
 							
-							map[oypos][oxpos] = ' ';
-							map[oypos][oxpos + mov] = 'O';
-							oxpos += mov;
+							if((oxpos + mov == xpos) && (oypos == ypos))
+							{
+								flag = 0;
+								break;
+							}
+
+							if (map[oypos][oxpos] == '$')
+								map[oypos][oxpos] = 'k';
 							
+							else map[oypos][oxpos] = ' ';
+							
+							if(map[oypos][oxpos + mov] == 'k')
+								map[oypos][oxpos + mov] = '$';
+							
+							else map[oypos][oxpos + mov] = '0';
+							
+							oxpos += mov;
+
 						}
 					}
 				}
-				
-				if ((Math.abs(oypos - ypos) == 1 && oxpos == xpos) || (Math.abs(oxpos - xpos) == 1 && oypos == ypos))
+
+				if (flag != 1 && ((Math.abs(oypos - ypos) == 1 && oxpos == xpos) || (Math.abs(oxpos - xpos) == 1 && oypos == ypos)))
 					flag = 0;
-				
+
 			}
-			
+
 		}
-		
+
 		System.out.print("\n\n\n\n\n\n\n");
 
 		for(int i = 0; i < map.length; i++) {	
@@ -263,17 +311,19 @@ public class Game
 
 		if(flag == 0)
 			System.out.println("GameOver!");
-		
-		s.close();
-		
+
 		return flag;
-		
+
 	}
-	
+
 	public static void main(String[] args) 
 	{
-		//if (level1() == 1)
-			if (level2() == 1);
-			System.out.println("Â¡Fuerte chico!");
+		Scanner s = new Scanner(System.in);
+
+		if (level1(s) == 1)
+			if (level2(s) == 1)
+				System.out.println("¡Fuerte chico!");
+
+		s.close();
 	}
 }
