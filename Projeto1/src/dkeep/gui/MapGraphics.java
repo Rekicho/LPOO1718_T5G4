e79a0c7cd94ae370968d220803064b8019f267f0
Pedro Neto <pneto.98@gmail.com>
2018-03-15 11:53:16 +0000

    Graphics

--- Projeto1/src/dkeep/gui/MapGraphics.java
+++ Projeto1/src/dkeep/gui/MapGraphics.java
@@ -2,14 +2,106 @@ package dkeep.gui;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 import dkeep.logic.Map;
 
 public class MapGraphics extends JPanel 
 {
-	Map mapa;
+	private Map mapa;
+	private BufferedImage hero;
+	private BufferedImage wall;
+	private BufferedImage guard;
+	private BufferedImage door;
+	private BufferedImage lever;
+	private BufferedImage guardasleep;
+	private BufferedImage matt;
+	private BufferedImage ogre;
+	private BufferedImage weapon;
+	private BufferedImage armedhero;
+	private BufferedImage overlap;
+	private BufferedImage herokey;
 	
-	MapGraphics(){}
+	
+	public MapGraphics() {
+		
+		try {
+			wall = ImageIO.read(new File("img/wall.png"));
+		} catch (IOException e) {
+			System.exit(1);
+		}
+		
+		try {
+			hero = ImageIO.read(new File("img/spody.png"));
+		} catch (IOException e) {
+			System.exit(1);
+		}
+		
+		try {
+			guard = ImageIO.read(new File("img/guard.png"));
+		} catch (IOException e) {
+			System.exit(1);
+		}
+		
+		try {
+			door = ImageIO.read(new File("img/door.png"));
+		} catch (IOException e) {
+			System.exit(1);
+		}
+		
+		try {
+			lever = ImageIO.read(new File("img/lever.png"));
+		} catch (IOException e) {
+			System.exit(1);
+		}
+		
+		try {
+			guardasleep = ImageIO.read(new File("img/guardasleep.png"));
+		} catch (IOException e) {
+			System.exit(1);
+		}
+		
+		try {
+			matt = ImageIO.read(new File("img/matt.png"));
+		} catch (IOException e) {
+			System.exit(1);
+		}
+		
+		try {
+			ogre = ImageIO.read(new File("img/ogre.png"));
+		} catch (IOException e) {
+			System.exit(1);
+		}
+		
+		try {
+			weapon = ImageIO.read(new File("img/weapon.png"));
+		} catch (IOException e) {
+			System.exit(1);
+		}
+		
+		try {
+			armedhero = ImageIO.read(new File("img/vader.png"));
+		} catch (IOException e) {
+			System.exit(1);
+		}
+		
+		try {
+			overlap = ImageIO.read(new File("img/pokeball.png"));
+		} catch (IOException e) {
+			System.exit(1);
+		}
+		
+		try {
+			herokey = ImageIO.read(new File("img/hero.png"));
+		} catch (IOException e) {
+			System.exit(1);
+		}
+		
+	}
 	
 	public void setMap(Map mapa)
 	{
@@ -29,21 +121,60 @@ public class MapGraphics extends JPanel
 		{
 			for(int y = 0; y < mapa.length(); y++)
 			{
-				if(mapa.position(x, y) == 'X')
-					g.fillRect(x*30, y*30, 30, 30);
 				
-				else if(mapa.position(x, y) == 'G')
-				{
-					g.setColor(Color.RED);
-					g.fillRect(x*30, y*30, 30, 30);
-					g.setColor(Color.BLACK);
-				}
-				
-				else if(mapa.position(x, y) == 'H')
-				{
-					g.setColor(Color.BLUE);
-					g.fillRect(x*30, y*30, 30, 30);
-					g.setColor(Color.BLACK);
+				switch(mapa.position(x, y)) {
+					case 'X':
+						g.drawImage(wall, x*30, y*30, this);
+						break;
+					
+					case 'G':
+						g.drawImage(guard, x*30, y*30, this);
+						break;
+						
+					case 'H':
+						g.drawImage(hero, x*30, y*30, this);
+						break;
+					
+					case 'k'	:
+						g.drawImage(lever, x*30, y*30, this);
+						break;
+						
+					case 'I'	:
+						g.drawImage(door, x*30, y*30, this);
+						break;
+						
+					case 'g':
+					case '8':
+						g.drawImage(guardasleep, x*30, y*30, this);
+						break;
+						
+					case 'S'	:
+						g.drawImage(matt, x*30, y*30, this);
+						break;
+						
+					case '0':
+						g.drawImage(ogre, x*30, y*30, this);
+						break;
+						
+					case 'A':
+						g.drawImage(armedhero, x*30, y*30, this);
+						break;
+						
+					case '*':
+						g.drawImage(weapon, x*30, y*30, this);
+						break;
+						
+					case 'K':
+						g.drawImage(herokey, x*30, y*30, this);
+						break;
+						
+					case '$':
+						g.drawImage(overlap, x*30, y*30, this);
+						break;
+						
+						
+					default:
+						break;
 				}
 			}
 		}
