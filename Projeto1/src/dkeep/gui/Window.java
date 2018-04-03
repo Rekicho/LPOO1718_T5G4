01961b70fc1a3759504a007a498dec725a688f88
Rekicho <up201604145@fe.up.pt>
2018-04-03 14:07:30 +0100

    Refactoring...

--- Projeto1/src/dkeep/gui/Window.java
+++ Projeto1/src/dkeep/gui/Window.java
@@ -21,6 +21,21 @@ public class Window {
 
 	private JFrame frame;
 	private JTextField textField;
+	private JLabel lblState;
+	private Edit edit;
+	private MapGraphics mapa;
+	private JLabel lblNumberOfOgres;
+	private JLabel lblGuardPersonality;
+	private JComboBox comboBox;
+	private JButton btnExit;
+	private JButton btnNewGame;
+	private JButton btnEditKeepLevel;
+	
+	JButton btnUp;
+	JButton btnDown;
+	JButton btnLeft;
+	JButton btnRight;
+	
 	private Game game;
 	private int state;
 	private Map level2map;
@@ -49,6 +64,28 @@ public class Window {
 		state = -1;
 		initialize();
 	}
+	
+	private void checkGameState()
+	{
+		if(state != 0) 
+		{
+			if (state == 1) {
+				lblState.setText("You Lost, Bitch!");
+			}
+			if (state == 2) {
+				lblState.setText("You Won!");
+			}
+			
+		game = null;
+		btnDown.setEnabled(false);
+		btnRight.setEnabled(false);
+		btnLeft.setEnabled(false);
+		btnUp.setEnabled(false);
+		}
+		mapa.setMap(game.getMap());
+		frame.repaint();
+		frame.requestFocusInWindow();
+	}
 
 	/**
 	 * Initialize the contents of the frame.
@@ -60,9 +97,9 @@ public class Window {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JLabel lblState = new JLabel("You can start a new game!");
+		lblState = new JLabel("You can start a new game!");
 		
-		Edit edit = new Edit(level2map);
+		edit = new Edit(level2map);
 		edit.setVisible(false);
 		edit.addWindowListener(new WindowAdapter() 
 		{
@@ -92,21 +129,21 @@ public class Window {
 	        });
 		
 		
-		MapGraphics mapa = new MapGraphics();
+		mapa = new MapGraphics();
 		mapa.setBounds(6, 68, 300, 300);
 		frame.getContentPane().add(mapa);
 		
-		JButton btnUp = new JButton("Up");
-		JButton btnDown = new JButton("Down");
-		JButton btnLeft = new JButton("Left");
-		JButton btnRight = new JButton("Right");
+		btnUp = new JButton("Up");
+		btnDown = new JButton("Down");
+		btnLeft = new JButton("Left");
+		btnRight = new JButton("Right");
 		
 		btnDown.setEnabled(false);
 		btnRight.setEnabled(false);
 		btnLeft.setEnabled(false);
 		btnUp.setEnabled(false);
 		
-		JLabel lblNumberOfOgres = new JLabel("Number of Ogres");
+		lblNumberOfOgres = new JLabel("Number of Ogres");
 		lblNumberOfOgres.setBounds(6, 17, 132, 16);
 		frame.getContentPane().add(lblNumberOfOgres);
 		
@@ -115,17 +152,17 @@ public class Window {
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
 		
-		JLabel lblGuardPersonality = new JLabel("Guard Personality");
+		lblGuardPersonality = new JLabel("Guard Personality");
 		lblGuardPersonality.setBounds(6, 45, 132, 16);
 		frame.getContentPane().add(lblGuardPersonality);
 		
 		String[] options = {"Rookie","Drunken","Suspicious"};
 		
-		JComboBox comboBox = new JComboBox(options);
+		comboBox = new JComboBox(options);
 		comboBox.setBounds(123, 45, 132, 16);
 		frame.getContentPane().add(comboBox);
 		
-		JButton btnExit = new JButton("Exit");
+		btnExit = new JButton("Exit");
 		btnExit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				System.exit(0);
@@ -137,24 +174,8 @@ public class Window {
 		btnLeft.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				state = game.gameLogic('a');
-				if(state != 0) 
-				{
-					if (state == 1) {
-						lblState.setText("You Lost, Bitch!");
-					}
-					if (state == 2) {
-						lblState.setText("You Won!");
-					}
-					
-				game = null;
-				btnDown.setEnabled(false);
-				btnRight.setEnabled(false);
-				btnLeft.setEnabled(false);
-				btnUp.setEnabled(false);
-				}
+				checkGameState();
 				mapa.setMap(game.getMap());
-				frame.repaint();
-				frame.requestFocusInWindow();
 			}
 		});
 		btnLeft.setBounds(327, 164, 84, 29);
@@ -163,23 +184,7 @@ public class Window {
 		btnUp.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				state = game.gameLogic('w');
-				if(state != 0) 
-				{
-					if (state == 1) {
-						lblState.setText("You Lost, Bitch!");
-					}
-					if (state == 2) {
-						lblState.setText("You Won!");
-					}
-					
-				game = null;
-				btnDown.setEnabled(false);
-				btnRight.setEnabled(false);
-				btnLeft.setEnabled(false);
-				btnUp.setEnabled(false);
-				}
-				frame.repaint();
-				frame.requestFocusInWindow();
+				checkGameState();
 			}
 		});
 		btnUp.setBounds(407, 124, 84, 29);
@@ -188,23 +193,7 @@ public class Window {
 		btnRight.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				state = game.gameLogic('d');
-				if(state != 0) 
-				{
-					if (state == 1) {
-						lblState.setText("You Lost, Bitch!");
-					}
-					if (state == 2) {
-						lblState.setText("You Won!");
-					}
-					
-				game = null;
-				btnDown.setEnabled(false);
-				btnRight.setEnabled(false);
-				btnLeft.setEnabled(false);
-				btnUp.setEnabled(false);
-				}
-				frame.repaint();
-				frame.requestFocusInWindow();
+				checkGameState();
 			}
 		});
 		btnRight.setBounds(490, 164, 84, 29);
@@ -213,23 +202,7 @@ public class Window {
 		btnDown.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				state = game.gameLogic('s');
-				if(state != 0) 
-				{
-					if (state == 1) {
-						lblState.setText("You Lost, Bitch!");
-					}
-					if (state == 2) {
-						lblState.setText("You Won!");
-					}
-				
-				game = null;
-				btnDown.setEnabled(false);
-				btnRight.setEnabled(false);
-				btnLeft.setEnabled(false);
-				btnUp.setEnabled(false);
-				}
-				frame.repaint();
-				frame.requestFocusInWindow();
+				checkGameState();
 			}
 		});
 		btnDown.setBounds(407, 204, 84, 29);
@@ -239,7 +212,7 @@ public class Window {
 		frame.getContentPane().add(lblState);
 
 		
-		JButton btnNewGame = new JButton("New Game");
+		btnNewGame = new JButton("New Game");
 		btnNewGame.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String ons = textField.getText();
@@ -278,7 +251,7 @@ public class Window {
 		btnNewGame.setBounds(399, 32, 117, 29);
 		frame.getContentPane().add(btnNewGame);
 		
-		JButton btnEditKeepLevel = new JButton("Edit Keep");
+		btnEditKeepLevel = new JButton("Edit Keep");
 		btnEditKeepLevel.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				frame.setVisible(false);
