d7279bd4605a17865909bd6def7ea491a78959f1
Rekicho <up201604145@fe.up.pt>
2018-04-05 11:55:31 +0100

    Fixed some tests

--- Projeto1/src/dkeep/test/TestOgreMovement.java
+++ Projeto1/src/dkeep/test/TestOgreMovement.java
@@ -16,20 +16,22 @@ public class TestOgreMovement {
 		int[] ogrePos, ogreCurrentPos;
 		boolean left = false, right = false, up = false, down = false;
 		int i = 0;
+		int state;
 		while(!(left && right && up && down) && i < 1000) {
 			ogrePos = game.getEnemies().get(0).getPosition().clone();
 			
 			if(i % 2 == 0)
-				game.level2('d',true);
+				state = game.level2('d',true);
 			else
-				game.level2('a',true);
+				state = game.level2('a',true);
 			
 			i++;
 			
 			ogreCurrentPos = game.getEnemies().get(0).getPosition();
 			
 			assertFalse(ogrePos == ogreCurrentPos);
-			assertFalse(Arrays.equals(ogrePos,ogreCurrentPos));
+			if(state != 1)
+				assertFalse(Arrays.equals(ogrePos,ogreCurrentPos));
 			
 			if (ogrePos[0] == ogreCurrentPos[0] + 1)
 				right = true;
@@ -50,19 +52,21 @@ public class TestOgreMovement {
 		int[] ogrePos, clubPos;
 		boolean left = false, right = false, up = false, down = false;
 		int i = 0;
+		int state;
 		while(!(left && right && up && down) && i < 1000) {
 			ogrePos = game.getEnemies().get(0).getPosition();
 			
 			if(i % 2 == 0)
-				game.level2('d',true);
+				state = game.level2('d',true);
 			else
-				game.level2('a',true);
+				state = game.level2('a',true);
 			
 			i++;
 			
 			clubPos = game.getWeapons().get(0).getPosition();
 	
-			assertFalse(Arrays.equals(ogrePos,clubPos));
+			if(state != 1)
+				assertFalse(Arrays.equals(ogrePos,clubPos));
 			
 			if (ogrePos[0] == clubPos[0] + 1)
 				right = true;
